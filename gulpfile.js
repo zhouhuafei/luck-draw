cdf8d7df0508d1b506457d2a0f66a640199a4ea4
zhouhuafei <1123486116@qq.com>
2018-12-01 08:52:25 +0800

    src

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,21 @@ const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
+const del = require('del');
+const replace = require('gulp-batch-replace'); // 内容替换(这个没问题，上个包出的问题，这个包没出类似的问题)
+const runSequence = require('run-sequence'); // 同步
 
 gulp.task('views', () => {
     gulp.src('./src/views/**/*.*')
         .pipe(plumber())
         .pipe(htmlmin({
-            collapseWhitespace: true,
+            collapseWhitespace: true, // 压缩HTML
+            removeComments: true, // 清除HTML注释
+            collapseBooleanAttributes: true, // 省略布尔属性的值 <input checked="true"/> ==> <input />
+            removeScriptTypeAttributes: true, // 删除<script>的type="text/javascript"
+            removeStyleLinkTypeAttributes: true, // 删除<style>和<link>的type="text/css"
+            minifyJS: true, //压缩页面JS
+            minifyCSS: true, //压缩页面CSS
         }))
         .pipe(gulp.dest('./dist/views/'));
 });
