c641418bcc6d3a0413a34d2e743dc0a253c9a5f5
zhouhuafei <1123486116@qq.com>
2018-12-01 08:12:14 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,32 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const plumber = require('gulp-plumber'); // 报错就中断？使用这个处理一下即可。
+const browserify = require('gulp-browserify');
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
 
-gulp.task('watch', function () {
-    gulp.watch([`./src/scss/**/*.scss`], ['scss']);
+gulp.task('views', () => {
+    gulp.src('./src/views/**/*.*')
+        .pipe(plumber())
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+        }))
+        .pipe(gulp.dest('./dist/views/'));
+});
+
+gulp.task('js', function () {
+    gulp.src('./src/js/**/*.js')
+        .pipe(plumber())
+        .pipe(babel({
+            presets: ['@babel/env'],
+        }))
+        .pipe(browserify())
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js/'));
 });
 
 gulp.task('scss', function () {
@@ -15,7 +38,22 @@ gulp.task('scss', function () {
             cascade: false,
         }))
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(`./src/css`));
+        .pipe(cssmin())
+        .pipe(gulp.dest(`./dist/css/`));
 });
 
-gulp.task('default', ['scss', 'watch']);
+gulp.task('images', function () {
+    gulp.src('./src/images/**/*.*')
+        .pipe(plumber())
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images/'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch([`./src/views/**/*.*`], ['views']);
+    gulp.watch([`./src/js/**/*.js`], ['js']);
+    gulp.watch([`./src/scss/**/*.scss`], ['scss']);
+    gulp.watch([`./src/images/**/*.*`], ['images']);
+});
+
+gulp.task('dev', ['views', 'js', 'scss', 'images', 'watch']);
