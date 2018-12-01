a8c37645d9537dbfe764cff7fee960e76a1cd34e
zhouhuafei <1123486116@qq.com>
2018-12-01 12:13:18 +0800

    ruleimg

--- src/js/index.js
+++ src/js/index.js
@@ -1,59 +1,125 @@
-function Game() {
-    this.init();
+const randomNum = require('zhf.random-num');
+const sku = require('zhf.sku');
+
+function px2rem(px) {
+    return px / 37.5 + 'rem';
 }
 
-Game.prototype.init = function () {
-    var domItems = document.querySelectorAll('.prize-item');
-    var domLamp = document.querySelector('.lamp');
-    var domHandle = document.querySelector('.handle');
-    var timer = null;
+class Game {
+    constructor() {
+        this.init();
+    }
 
-    function selected() {
-        domItems.forEach(function (v, i) {
-            v.classList.add('prize-item_selected4');
+    init() {
+        const domItems = document.querySelectorAll('.prize-item');
+        const domLamp = document.querySelector('.lamp');
+        const domHandle = document.querySelector('.handle');
+        const $remainderNum = $('.btn-remainder-num');
+        const $tel = $('.tel');
+        const $rule = $('.rule');
+        const prizeH = 267 / 2; // 一个奖品的高度
+        const prizeNum = 5; // 奖品数量
+        const prizeAllH = prizeH * prizeNum; // 奖品总高度
+        const prizeScroll = 15; // 奖品滚动基数
+        const handleMoveTime = 200; // 摇杆的动画间隔时间
+        let remainder = 3; // 剩余抽奖次数
+        let timer = null;
+        let isClick = false; // 防止重复点击
+        const noPrize = sku([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]);
+        noPrize.forEach(function (v, i, a) {
+            if (v[0] === v[1] && v[0] === v[2]) {
+                a.splice(i, 1);
+            }
         });
-    }
+        $('.rule-btn').on('click', function () {
+            $rule.addClass('rule_show');
+        });
+        $('.handle-btn,.btn').on('click', function () {
+            if (remainder === 0) {
+                alert('您今天的抽奖次数用完了!');
+                return;
+            }
+            if (!(/1\d{10}/.test($tel.val()))) {
+                alert('请输入正确的11位手机号码!');
+                return;
+            }
+            fnHandleMove();
+            if (!isClick) {
+                isClick = true;
+                $.ajax({
+                    url: '/luck-draw_scroll/dist/views/index.html', // 待续...
+                    method: 'get', // 待续...
+                    data: {
+                        tel: $tel.val(),
+                    },
+                    // dataType: 'jsonp', // 待续...
+                    success: function (res) {
+                        const level = 3; // 待续...
+                        // const remainder = response.remainder; // 待续...
+                        if (remainder === 0) {
+                            alert('您今天的抽奖次数用完了!');
+                            isClick = false;
+                            return;
+                        }
+                        let levelResult = [];
+                        if (level === 0) { // 没中奖
+                            const noPrizeRandomOne = noPrize[randomNum(0, noPrize.length - 1)];
+                            levelResult = levelResult.concat(noPrizeRandomOne);
+                        } else {
+                            for (let i = 0; i < 3; i++) {
+                                levelResult.push(level);
+                            }
+                        }
+                        selected(levelResult, remainder);
+                        fnLampMove();
+                        setTimeout(function () {
+                            fnLampStop();
+                            fnHandleStop();
+                            isClick = false;
+                        }, 3400);
+                        remainder--; // 删除待续...
+                        $remainderNum.html(remainder);
+                    },
+                });
+            }
+        });
 
-    setTimeout(function () {
-        fnHandleMove();
-        setTimeout(function () {
-            selected();
-            fnLampMove();
+        function selected(levelResult, remainder) {
+            domItems.forEach(function (v, i) {
+                v.style.backgroundPosition = `0 ${px2rem(-((3 - remainder + 1) * prizeScroll * prizeAllH + prizeH * (levelResult[i] - 1)))}`;
+            });
+        }
+
+        function fnLampMove() {
+            timer = setInterval(function () {
+                domLamp.classList.toggle('lamp_selected');
+            }, 80);
+        }
+
+        function fnLampStop() {
+            clearInterval(timer);
+        }
+
+        function fnHandleMove() {
             setTimeout(function () {
-                fnLampStop();
-                fnHandleStop();
-            }, 3000);
-        }, 600);
-    }, 1000);
-
-
-    function fnLampMove() {
-        timer = setInterval(function () {
-            domLamp.classList.toggle('lamp_selected');
-        }, 80);
-    }
-
-    function fnLampStop() {
-        clearInterval(timer);
-    }
-
-    function fnHandleMove() {
-        var time = 200;
-        setTimeout(function () {
-            domHandle.classList.remove('handle_selected1');
-            domHandle.classList.add('handle_selected2');
+                domHandle.classList.remove('handle_selected1');
+                domHandle.classList.add('handle_selected2');
+            }, handleMoveTime);
             setTimeout(function () {
                 domHandle.classList.remove('handle_selected2');
                 domHandle.classList.add('handle_selected3');
-            }, time);
-        }, time);
-    }
+            }, handleMoveTime * 2);
+        }
 
-    function fnHandleStop() {
-        domHandle.classList.add('handle_selected1');
-        domHandle.classList.remove('handle_selected2');
-        domHandle.classList.remove('handle_selected3');
+        function fnHandleStop() {
+            domHandle.classList.remove('handle_selected3');
+            domHandle.classList.add('handle_selected2');
+            setTimeout(function () {
+                domHandle.classList.remove('handle_selected2');
+                domHandle.classList.add('handle_selected1');
+            }, handleMoveTime);
+        }
     }
-};
+}
 
 new Game();
