075fbfa4e565a5b0e6961cda6a23fa0caea37442
zhouhuafei <1123486116@qq.com>
2018-12-02 23:30:36 +0800

    build

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
-const plumber = require('gulp-plumber'); // 报错就中断？使用这个处理一下即可。
+const plumber = require('gulp-plumber');
 const browserify = require('gulp-browserify');
 const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
@@ -10,11 +10,44 @@ const uglify = require('gulp-uglify');
 const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
 const del = require('del');
-const replace = require('gulp-batch-replace'); // 内容替换(这个没问题，上个包出的问题，这个包没出类似的问题)
-const runSequence = require('run-sequence'); // 同步
+const replace = require('gulp-batch-replace');
+const through = require('through2');
+const runSequence = require('run-sequence'); // 同步(要保证task中return了一个Promise，否则无效)
+const rev = require('gulp-rev'); // 生成md5文件以及生成md的映射文件
+const revCollector = require('gulp-rev-collector'); // 替换html中引入的文件名(css，js，images)。替换css中引入的文件名(images)。也就是说此包可以用来替换被引入文件的路径以及文件名。
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const entryPath = `./src`;
+const outputPath = `./dist`;
+const outputPathTemporary = `./public/static/dist-temporary`;
+const dirReplacementsDev = { // 对css和html中的进行路径替换
+    html: [
+        ['css/', '../css/'],
+        ['js/', '../js/'],
+        ['images/', '../images/'],
+    ],
+    css: [
+        ['images/', '../images/'],
+    ],
+};
+const dirReplacementsBuildViews = { // 对views中的进行路径替换
+    'css/': '../css/',
+    'js/': '../js/',
+    'images/': '../images/',
+};
+const dirReplacementsBuildCss = { // 对css中的进行路径替换
+    'images/': '../images/',
+};
 
-gulp.task('views', () => {
-    gulp.src('./src/views/**/*.*')
+gulp.task('dev-views', () => {
+    return gulp.src(`${entryPath}/views/**/*.*`)
+        .pipe(plumber())
+        .pipe(replace(dirReplacementsDev.html))
+        .pipe(gulp.dest(`${outputPath}/views/`));
+});
+
+gulp.task('build-views', () => {
+    return gulp.src(`${entryPath}/views/**/*.*`)
         .pipe(plumber())
         .pipe(htmlmin({
             collapseWhitespace: true, // 压缩HTML
@@ -25,44 +58,108 @@ gulp.task('views', () => {
             minifyJS: true, //压缩页面JS
             minifyCSS: true, //压缩页面CSS
         }))
-        .pipe(gulp.dest('./dist/views/'));
+        .pipe(gulp.dest(`${outputPathTemporary}/views/`));
 });
 
-gulp.task('js', function () {
-    gulp.src('./src/js/**/*.js')
+gulp.task('dev-js', function () {
+    return gulp.src(`${entryPath}/js/**/*.js`)
         .pipe(plumber())
         .pipe(babel({
             presets: ['@babel/env'],
         }))
         .pipe(browserify())
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist/js/'));
+        .pipe(gulp.dest(`${outputPath}/js/`));
 });
 
-gulp.task('scss', function () {
-    gulp.src(`./src/scss/**/*.scss`)
+gulp.task('build-js', function () {
+    return gulp.src(`${entryPath}/js/**/*.js`)
+        .pipe(babel({
+            presets: ['@babel/env'],
+        }))
+        .pipe(browserify())
+        .pipe(uglify())
+        .pipe(rev())
+        .pipe(gulp.dest(`${outputPath}/js/`))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest(`${outputPathTemporary}/js/`));
+});
+
+gulp.task('dev-scss', function () {
+    return gulp.src(`${entryPath}/scss/**/*.scss`)
         .pipe(plumber())
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false,
         }))
         .pipe(sass().on('error', sass.logError))
+        .pipe(replace(dirReplacementsDev.css))
+        .pipe(gulp.dest(`${outputPath}/css/`));
+});
+
+gulp.task('build-scss', function () {
+    return gulp.src(`${entryPath}/scss/**/*.scss`)
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false,
+        }))
+        .pipe(sass().on('error', sass.logError))
         .pipe(cssmin())
-        .pipe(gulp.dest(`./dist/css/`));
+        .pipe(rev())
+        .pipe(gulp.dest(`${outputPathTemporary}/css/`))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest(`${outputPathTemporary}/css/`));
 });
 
-gulp.task('images', function () {
-    gulp.src('./src/images/**/*.*')
+gulp.task('dev-images', function () {
+    return gulp.src(`${entryPath}/images/**/*.*`)
         .pipe(plumber())
+        .pipe(gulp.dest(`${outputPath}/images/`));
+});
+
+gulp.task('build-images', function () {
+    return gulp.src(`${entryPath}/images/**/*.*`)
         .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images/'));
+        .pipe(rev())
+        .pipe(gulp.dest(`${outputPath}/images/`))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest(`${outputPathTemporary}/images/`));
 });
 
-gulp.task('watch', function () {
-    gulp.watch([`./src/views/**/*.*`], ['views']);
-    gulp.watch([`./src/js/**/*.js`], ['js']);
-    gulp.watch([`./src/scss/**/*.scss`], ['scss']);
-    gulp.watch([`./src/images/**/*.*`], ['images']);
+gulp.task('build-rev-views', function () {
+    return gulp.src([`${outputPathTemporary}/**/*.json`, `${outputPathTemporary}/views/**/*.*`])
+        .pipe(revCollector({
+            replaceReved: true,
+            dirReplacements: dirReplacementsBuildViews,
+        }))
+        .pipe(gulp.dest(`${outputPath}/views/`));
 });
 
-gulp.task('dev', ['views', 'js', 'scss', 'images', 'watch']);
+gulp.task('build-rev-css', function () {
+    return gulp.src([`${outputPathTemporary}/**/*.json`, `${outputPathTemporary}/css/**/*.css`])
+        .pipe(revCollector({
+            replaceReved: true,
+            dirReplacements: dirReplacementsBuildCss,
+        }))
+        .pipe(gulp.dest(`${outputPath}/css/`));
+});
+
+gulp.task('del-dist', function () {
+    return del([outputPath]);
+});
+
+gulp.task('del-dist-temporary', function () {
+    return del([outputPathTemporary]);
+});
+
+gulp.task('dev-watch', function () {
+    gulp.watch([`${entryPath}/views/**/*.*`], ['dev-views']);
+    gulp.watch([`${entryPath}/js/**/*.js`], ['dev-js']);
+    gulp.watch([`${entryPath}/scss/**/*.scss`], ['dev-scss']);
+    gulp.watch([`${entryPath}/images/**/*.*`], ['dev-images']);
+});
+
+gulp.task('dev', ['dev-views', 'dev-js', 'dev-scss', 'dev-images', 'dev-watch']);
+
+gulp.task('build', function (done) {
+    runSequence(['del-dist'], ['build-views', 'build-js', 'build-scss', 'build-images'], ['build-rev-views', 'build-rev-css'], ['del-dist-temporary'], done);
+});
